f0e62c8095b4ce7e1b70e03af9284079400867aa
Luis <usr3981@salesianos-lacuesta.net>
2025-05-16 07:55:18 +0100

    feature(App.java): creado el arraylist que contrendra los elementos

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ import net.salesianos.menus.MenuPrincipal;
 
 public class App {
 
-    // protected static ArrayList<Elemento> listaElementos = new ArrayList<>();
+    protected static ArrayList<Elemento> listaElementos = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
         MenuPrincipal menuPrincipal = new MenuPrincipal();
